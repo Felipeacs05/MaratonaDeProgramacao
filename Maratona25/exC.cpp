024681fdf0a02fb3db49aad8c6332299dd3f0b25
Felipeacs05 <felipedocarmo05@gmail.com>
2025-09-20 13:10:02 -0300

    Manipulação de String em c++

--- Maratona25/exC.cpp
+++ Maratona25/exC.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main(){
+    int qntd;
+    char frase[100];
 
+    cout<<"Digite os baguhos:";
+    gets(frase);
+    cout<<frase[0];
+    cout<<"Esta frase tem" << strlen(frase)<< " caracteres" <<endl;
+    for(auto character : frase){
+        cout << character;
+        cout << endl;
+    }
     
+
+    return 0;
 }
