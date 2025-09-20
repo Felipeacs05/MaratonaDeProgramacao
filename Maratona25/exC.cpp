f23b033b0d4503717dab5cb4fb6057de69459a87
Felipeacs05 <felipedocarmo05@gmail.com>
2025-09-20 18:41:10 -0300

    Loop inicial para resolução exercício C em C++

--- Maratona25/exC.cpp
+++ Maratona25/exC.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <sstream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -13,16 +14,22 @@ int main(){
     getline(cin, line); // Lê a linha inteira, incluindo os espaços
 
     stringstream ss(line); // Cria um stream a partir da linha lida
-    vector<int> phrase(100); // Vetor para armazenar os números
+    vector<int> phrase; // Vetor para armazenar os números
 
     int num;
     while(ss>>num){// Lê cada número do stream
         phrase.push_back(num);
     }
-
-    for(int i=0; i<size(phrase); i++){
-        cout << phrase[i] << " ";
+    
+    //Agora que temos os números em um vetor, podemos iterar sobre ele
+    int total;
+    int count;
+    for (int i = qntd; i>=0; i--){
+        if (phrase[i]){
+            total = total + pow(2, count);
+        }
     }
+
     /**for(auto character : frase){
         cout << character;
         cout << endl;
