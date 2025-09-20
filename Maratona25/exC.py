31e144037b8473e91bbe2670a6ea43629214cd09
Felipeacs05 <felipedocarmo05@gmail.com>
2025-09-20 12:14:53 -0300

    Respondendo questão C

---
+++ Maratona25/exC.py
@@ -0,0 +1,27 @@
+qntd = int(input(""))
+n = input("")
+
+listn = list(map(int, n.split()))
+
+count = 0
+
+for i in range(qntd, -1, -1):
+    if listn[i] == True:
+        total = total + 2**count
+    count+=1
+
+contador = 0
+
+while True:
+    if total != 1:
+        if total%2==0:
+            total = total //2
+            contador+=1
+        else:
+            ref = total
+            total = total*2 + 1
+            total (total*2 + 1)^total
+    else:
+        break
+
+print(contador)
